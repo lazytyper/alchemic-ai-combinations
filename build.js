b1fdb1de0dae5abe7b75eb51524113f865ae269b
Egon Schmid <eschmid1972@gmail.com>
2025-01-30 21:14:17 +0100

    faster building script

--- build.js
+++ build.js
@@ -6,111 +6,52 @@ const combinations = {};
 
 let autoId = 1;
 
-const startItems = [
-    'air',
-    'earth',
-    'fire',
-    'water'
-];
-
 function addItem(name) {
-    let id = itemNames[name];
-    if (id === undefined) {
-        id = autoId++;
-        itemNames[name] = id;
-        items[id] = name;
+    let o = itemNames[name];
+    if (o === undefined) {
+        o = {
+            id: autoId++,
+            name,
+            create: [],
+            use: [],
+        };
+        itemNames[name] = o;
+        items[o.id] = o;
     }
-    return id;
+    return o;
 }
 
 function addCombination(names, resultName) {
-    let ids = names.map(addItem);
-    let resultId = addItem(resultName);
-
-    ids.sort((a, b) => a - b);
-
-    let key = ids.join('_');
-
-    if (combinations[key] === undefined) {
-        combinations[key] = [resultId];
+    let keyItems = names.map(addItem);
+    let resultId = addItem(resultName).id;
+    let keyIds = keyItems.map(item => item.id);
+    let combinationKey = keyIds.join('_');
+    let comb;
+    if (combinations[combinationKey] === undefined) {
+        comb = combinations[combinationKey] = [resultId];
     } else {
-        if (combinations[key].includes(resultId)) {
+        comb = combinations[combinationKey];
+        if (comb.includes(resultId)) {
             console.warn(`Duplicate combination: ${names.join(' + ')} = ${resultName}`);
-
+            return;
         }
-        combinations[key].push(resultId);
+        comb.push(resultId);
     }
+    let keyItems2 = Array.from(new Set(keyIds));
+    for (let key of keyItems2) {
+        items[key].use.push(combinationKey);
+    }
+    items[resultId].create.push(combinationKey);
 }
 
-function listCombinationsWith(name) {
-    let result = { creations: [], usages: [] };
-    let id = itemNames[name];
-    for (let key in combinations) {
-        let ids = key.split('_').map(id => parseInt(id));
-        let resultIds = combinations[key];
-        let resultNames = ids.map(id => Object.keys(itemNames).find(key => itemNames[key] == id));
-
-        if (resultIds.includes(id)) {
-            let creation = { names: resultNames, resultName: name };
-            result.creations.push(creation);
-        }
-
-        if (ids.includes(id)) {
-            for (let resultId of resultIds) {
-                let usage = { names: resultNames, resultName: items[resultId] };
-                result.usages.push(usage);
-            }
-        }
-
-    }
-    return result;
-}
-
-function getUnusedItems() {
-    let result = [];
-    for (let key in itemNames) {
-        let id = itemNames[key];
-        let found = false;
-        for (let key2 in combinations) {
-            let ids = key2.split('_');
-            if (ids[0] == id || ids[1] == id) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            result.push(key);
-        }
-    }
-    return result;
-}
-
-function getMissingCombinations() {
-
-    let itemSet = new Set();
-    for (let key in itemNames) {
-        itemSet.add(itemNames[key]);
-    }
-
-    startItems.map(name => itemNames[name]).forEach(id => itemSet.delete(id));
-
-    const key = Object.keys(combinations);
-
-    for (let k of key) {
-        let ids = combinations[k];
-        for (let id of ids) {
-            itemSet.delete(id);
-        }
-    }
-
-    return Array.from(itemSet).map(id => items[id]);
+function getItemsSortedByNames() {
+    return Object.values(items).sort((a, b) => a.name.localeCompare(b.name));
 }
 
 function formatElement(name) {
     name = name.split('-').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join('-');
     return name.split('_').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
 }
-
 
 function addline(string) {
     let names = string.split(',');
@@ -119,13 +60,60 @@ function addline(string) {
     addCombination(names, resultName);
 }
 
-function createHTML() {
-    const itemIds = Object.keys(itemNames).sort().map(key => itemNames[key]);
-    function ref(name) {
-        return '<a href="#' + name + '">' + formatElement(name) + '</a>';
+// read data file
+const dataContent = require('fs').readFileSync('data.txt', 'utf-8');
+const lines = dataContent.split('\n');
+for (let line of lines) {
+    line = line.trim();
+    if (line.startsWith('#')) continue;
+    if (line === '') continue;
+    addline(line);
+}
+
+const itemsSorted = getItemsSortedByNames();
+
+itemsSorted.forEach(item => {
+    const { name, create, use } = item;
+    let content = [
+        '==================================',
+        '= ' + formatElement(name),
+        '==================================',
+        ''
+    ];
+
+    content.push('created by:');
+    content.push('===========')
+    for (let c of create) {
+        const ids = c.split('_');
+        const names = ids.map(id => items[id].name);
+        content.push(names.map(formatElement).join(' + ') + ' = ' + formatElement(name));
     }
-    const preHTML = `
-<!DOCTYPE html>
+
+    content.push('');
+
+    content.push('Usages:');
+    content.push('=======');
+
+    for (let c of use) {
+        const ids = c.split('_');
+        const names = ids.map(id => items[id].name);
+        const resultNames = combinations[c].map(id => items[id].name);
+        for (let resultName of resultNames) {
+            content.push(names.map(formatElement).join(' + ') + ' = ' + formatElement(resultName));
+        }
+
+        fs.writeFileSync(`stat/items/${name}.txt`, content.join('\n'));
+
+
+    }
+});
+
+function ref(name) {
+    return '<a href="#' + name + '">' + formatElement(name) + '</a>';
+}
+
+function createHTML() {
+    const preHTML = `<!DOCTYPE html>
 <html>
 <head>
     <title>Alchemy</title>
@@ -159,44 +147,49 @@ function createHTML() {
         .index a { margin-left: 5px; margin-right: 5px; }
     </style>
 </head>
-<body>`;
-
+<body>`; // end of preHTML
+    // `
     const blocks = [];
 
     blocks.push('<h1>AlchemicAI Combination List</h1>');
     blocks.push('<div class="index">');
-    blocks.push(`<h2>Index (${itemIds.length} Items)</h2>`);
+    blocks.push(`<h2>Index (${itemsSorted.length} Items)</h2>`);
 
-    for (let id of itemIds) {
-        const name = items[id];
+    for (let item of itemsSorted) {
+        const name = item.name;
         blocks.push(`<a class="element" href="#${name}">${formatElement(name)}</a>`);
     }
 
     blocks.push('</div>');
 
-    for (let id of itemIds) {
-        const name = items[id];
-        const combinations = listCombinationsWith(name);
-
+    for (let item of itemsSorted) {
+        const { name, create, use } = item;
         let content = [
-            `
-            <a name="${name}"></a>
-            <h2>${formatElement(name)}</h2>`,
-            '<h3>Created by:</h3>',
-            '<ul>'
+            `<a name="${name}"></a>
+            <h2>${formatElement(name)}</h2>`
+
         ];
-        for (let c of combinations.creations) {
-            content.push(`<li>${c.names.map(ref).join(' + ')} = ${ref(c.resultName)}</li>`);
+
+        content.push('<h3>Created by:</h3>');
+        content.push('<ul>');
+        for (let c of create) {
+            const ids = c.split('_');
+            const names = ids.map(id => items[id].name);
+            content.push(`<li>${names.map(ref).join(' + ')} = ${ref(name)}</li>`);
         }
         content.push('</ul>');
-        if (combinations.usages.length > 0) {
-            content.push('<h3>Usages:</h3>');
-            content.push('<ul>');
-            for (let c of combinations.usages) {
-                content.push(`<li>${c.names.map(ref).join(' + ')} = ${ref(c.resultName)}</li>`);
+
+        content.push('<h3>Usages:</h3>');
+        content.push('<ul>');
+        for (let c of use) {
+            const ids = c.split('_');
+            const names = ids.map(id => items[id].name);
+            const resultNames = combinations[c].map(id => items[id].name);
+            for (let resultName of resultNames) {
+                content.push(`<li>${names.map(ref).join(' + ')} = ${ref(resultName)}</li>`);
             }
-            content.push('</ul>');
         }
+        content.push('</ul>');
 
         blocks.push(content.join('\n'));
     }
@@ -206,52 +199,12 @@ function createHTML() {
     fs.writeFileSync('stat/index.html', preHTML + blocks.join('\n') + postHTML);
 }
 
-// read data file
-const dataContent = require('fs').readFileSync('data.txt', 'utf-8');
-const lines = dataContent.split('\n');
-for (let line of lines) {
-    line = line.trim();
-    if (line.startsWith('#')) continue;
-    if (line === '') continue;
-    addline(line);
-}
+// missing items
+const itemsWithNoCreate = itemsSorted.filter(item => item.create.length === 0 && !(['air', 'earth', 'fire', 'water'].includes(item.name)));
 
-// create item files
-const itemIds = Object.keys(items);
-itemIds.forEach(id => {
-    const name = items[id];
-    const combinations = listCombinationsWith(name);
 
-    let content = [
-        '==================================',
-        '= ' + formatElement(name),
-        '==================================',
-        ''
-    ];
-
-    content.push('created by:');
-    content.push('===========')
-    for (let c of combinations.creations) {
-        content.push(c.names.map(formatElement).join(' + ') + ' = ' + formatElement(c.resultName));
-    }
-    content.push('');
-    content.push('Usages:');
-    content.push('=======')
-    for (let c of combinations.usages) {
-        content.push(c.names.map(formatElement).join(' + ') + ' = ' + formatElement(c.resultName));
-    }
-
-    fs.writeFileSync(`stat/items/${name}.txt`, content.join('\n'));
-});
-
-// create file for unknown items
-const missing = getMissingCombinations();
-let content;
-if (missing.length) {
-    content = getMissingCombinations().map(formatElement).join('\n');
-} else {
-    content = 'No missing combinations';
-}
+const content = itemsWithNoCreate.map(item => item.name).join('\n');
 fs.writeFileSync('stat/missing.txt', content);
+
 
 createHTML();
