960e33b05dba51a16333eb7a8e3e258079a1cb73
Egon Schmid <eschmid1972@gmail.com>
2025-03-20 01:18:31 +0100

    more words + fixes

--- build2.js
+++ build2.js
@@ -13,7 +13,7 @@ blocks.push(JSON.stringify({
 blocks.push('\n');
 blocks.push(fs.readFileSync('./public/app.js', 'utf8'));
 
-blocks.push('</script></head><body><div class="container"><h1>Alchemic AI elements search</h1><p> <span id="element-count">0</span> elements available</p><div id="inputform"><input id="search" type="text" placeholder="element" autocomplete="off"><button id="searchbtn">Search</button> <label style="margin-left:2em">Search method:</label> <select id="search-method"><option value="0">Fuzzy search</option><option value="1">Contains term (like in game)</option><option value="2">Begins with term (like in game)</option><option value="3">Ends with term</option></select></div><div id="solutions"></div></div></body></html>');
+blocks.push('</script></head><body><div class="container"><h1>Alchemic AI elements search</h1><p> <span id="element-count">0</span> elements available</p><div id="inputform"><input id="search" type="text" placeholder="element" autocomplete="off"><button id="searchbtn">Show</button> <label style="margin-left:2em">Search method:</label> <select id="search-method"><option value="0">Fuzzy search</option><option value="1">Contains term (like in game)</option><option value="2">Begins with term (like in game)</option><option value="3">Ends with term</option></select></div><div id="solutions"></div></div></body></html>');
 
 fs.writeFileSync('alchemic.html', blocks.join(''));
 
