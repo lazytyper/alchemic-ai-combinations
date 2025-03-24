c206576b8d71231a0e767cd51df4ae7496994389
Egon Schmid <eschmid1972@gmail.com>
2025-03-24 13:14:47 +0100

    backup

--- public/app.js
+++ public/app.js
@@ -217,8 +217,10 @@ function formatBack(name) {
 }
 
 function formatElement(name) {
-    name = name.split('-').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join('-');
-    return name.split('_').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
+    name = name.replace(/_/g, ' ');
+    return name.toLowerCase().replace(/(^|[0-9\- ])([a-z])/g, (match, p1, p2) => {
+        return p1 + p2.toUpperCase();
+    });
 }
 
 function addCombination(combination, el) {
